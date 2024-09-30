71c70153e95ffde643cac0ab655ef439c0e195b5
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-09-30 18:27:12 +0530

    Add files via upload

---
+++ Class_1.py
@@ -0,0 +1,33 @@
+'''In today's class :
+1. expressions
+    single line
+    multi line
+2. comments
+    single line
+    multi line'''
+
+
+#single line expression - use single inverted comma
+print("Hello..")
+
+'''Multi line expression -
+use 3 pair of inverted comma'''
+
+print("""Hello world..
+      sdcnjdfvbujfd
+      jbfvyuhjbfdvb
+      jbjcuixdfhvnm
+      """)
+
+x = 5
+y = 3
+z = x + y
+print("Sum of x and y is:",z)
+print("Subtraction of x and y",x-y)
+print("Multiplication of x and y",x*y)
+print("Division of x and y:",x/y)
+print("Floor division of x and y:",x//y)
+print("Modulus of x and y is: ",x%y)
+print("y is the Power of x :",x**y)
+
+
