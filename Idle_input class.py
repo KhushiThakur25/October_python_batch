0fdec48b3967cc44abf8c6fabe23b24221793b2c
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-10-04 18:17:38 +0530

    Add files via upload

---
+++ Idle_input class.py
@@ -0,0 +1,21 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/October_batch/Print_statements.py
+You are not allowed to join..
+>>> n = input("Enter the val1")
+Enter the val155
+>>> m = input("Enter the val2")
+Enter the val222
+>>> n + m
+'5522'
+>>> 5 + 2
+7
+>>> "Riya" + " Sharma"
+'Riya Sharma'
+>>> n = int(input("Enter the val1"))
+Enter the val155
+>>> m = int(input("Enter the val2"))
+Enter the val222
+>>> n + m
+77
---
+++ Print_statements.py
@@ -0,0 +1,23 @@
+#printstatements...
+'''print("Hello")
+x = 5
+y = 6
+print("Sum of x and y is:",x+y)
+print("Sum of",x , "and ",y ,"is :",x+y)
+print("Sum of %d and %d is %d"%(x,y,x+y))
+print("Div of %d and %d is %f"%(x,y,x/y))
+print("Sum of {} and {} is {}".format(x,y,x+y))
+print("Div of {} and {} is {}".format(x,y,x/y))
+print(f"Div of {x} and {y} is {x/y}")'''
+
+
+# conditional statments:
+'''if - else
+if - elif - else
+if
+nested if- else'''
+invitation = False
+if invitation:
+    print("Welcome to party!")
+else:
+    print("You are not allowed to join..")
